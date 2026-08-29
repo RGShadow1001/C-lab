9ce4181676dafd3d549773a710b2135de4967b61
Shaurya Pratap Singh <rgshadow1001@gmail.com>
2026-08-29 21:29:11 +0530

    my base code

---
+++ basic-codes/helloworld.c
@@ -0,0 +1,9 @@
+//
+// Created by rgshadow on 4/29/26.
+//
+
+#include <stdio.h>
+void main()
+{
+      printf("My 1st code");
+}
--- basic-codes/helloworld.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by rgshadow on 4/29/26.
-//
-
-#include "helloworld.h"
